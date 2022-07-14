7210232f3a283c3e27e605daa8fcf3e3cdb4bb6f
Matheus Sodre <matheusssodre98@gmail.com>
2022-07-14 15:09:49 -0300

    firts commit

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,29 +1,91 @@
 import "./App.css";
 import React from "react";
-import ComParametro from "./components/ComParametro";
-import PrimeiroComponente from "./components/PrimeiroComponente";
-import ComFilhos from "./components/ComFilhos";
+
 import Card from "./components/layout/Card";
 
-export default (props) => (
-  <div className="App">
-    <Card titulo="Primeiro Componente">
-      <PrimeiroComponente />
-    </Card>
+import Mega from "./components/mega/Mega";
+import Contador from "./components/contador/Contador";
+import Input from "./components/formulario/Input";
+import IndiretaPai from "./components/comunicacao/IndiretaPai";
+import DiretaPai from "./components/comunicacao/DiretaPai";
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import ParOuImpar from "./components/condicional/ParOuImpar";
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Fragmento from "./components/basicos/Fragmento";
+import ComParametro from "./components/basicos/ComParametro";
+import Primeiro from "./components/basicos/Primeiro";
 
-    <Card titulo="Componente com Parametro">
-      <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo" />
-    </Card>
+export default () => (
+    <div className="App">
+        <h1>Fundamentos React</h1>
 
-    <Card titulo="Componente com Filhos">
-      <ComFilhos>
-        <ul>
-          <li>Ana</li>
-          <li>Bia</li>
-          <li>Carlos</li>
-          <li>Daniel</li>
-        </ul>
-      </ComFilhos>
-    </Card>
-  </div>
+        <div className="Cards">
+            <Card titulo="#13 - Mega" color="#B9006E">
+                <Mega qtde={8}></Mega>
+            </Card>
+
+            <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial={10}></Contador>
+            </Card>
+
+            <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
+                <Input></Input>
+            </Card>
+
+            <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+                <IndiretaPai></IndiretaPai>
+            </Card>
+
+            <Card titulo="#09 - Comunicação Direta" color="#59323C">
+                <DiretaPai></DiretaPai>
+            </Card>
+
+            <Card titulo="#08 - Renderização Condicional" color="#982395">
+                <ParOuImpar numero={21}></ParOuImpar>
+                <UsuarioInfo usuario={{ nome: "Fernando" }} />
+                {/* <UsuarioInfo usuario={{}} />
+                <UsuarioInfo /> */}
+            </Card>
+
+            <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+                <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
+                </Familia>
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+    </div>
 );
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
